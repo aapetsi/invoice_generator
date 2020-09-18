ae9f6eb4dfb613b5980c59826729ab37a1b78bdc
aapetsi <apetsiampiah@gmail.com>
2020-09-18 07:09:59 +0000

    order invoice by employee id

--- app/controllers/api/v1/invoice_controller.rb
+++ app/controllers/api/v1/invoice_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class InvoiceController < ApplicationController
       def show
-        timesheets = Timesheet.select("employee_id, billable_rate, SUM(billable_rate * total_hours) as total_cost, SUM(total_hours) as number_of_hours").where(company: params[:id]).group("employee_id, billable_rate")
+        timesheets = Timesheet.select("employee_id, billable_rate, SUM(billable_rate * total_hours) as total_cost, SUM(total_hours) as number_of_hours").where(company: params[:id]).group("employee_id, billable_rate").order("employee_id ASC")
         
         # Calculate total cost
         total_cost = 0.0
